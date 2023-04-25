99df8e803bd7e8bf8a6fc1aeecf44d5293d89ace
Marianna <Marianna.l.americo@gmail.com>
2023-04-25 16:37:20 -0300

    Atividade prática 1

---
+++ CBBAtividade1.java
@@ -0,0 +1,10 @@
+public class CBBAtividade1 {
+    public static void main (String[] args){
+        int ages[];
+        ages = new int[100];
+        for (int i = 0; i < 100; i++){
+            ages[i] = i;
+            System.out.print(ages[i] + " ");
+        }
+    }
+}
